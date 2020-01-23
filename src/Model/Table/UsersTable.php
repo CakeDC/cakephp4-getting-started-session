79dc232ba31a9900e11b9489a74973f0fe997b55
Jorge González <steinkel@gmail.com>
2020-01-23 13:32:58 +0000

    fix display field to email in Users Table

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -38,7 +38,7 @@ class UsersTable extends Table
         parent::initialize($config);
 
         $this->setTable('users');
-        $this->setDisplayField('id');
+        $this->setDisplayField('email');
         $this->setPrimaryKey('id');
 
         $this->addBehavior('Timestamp');
